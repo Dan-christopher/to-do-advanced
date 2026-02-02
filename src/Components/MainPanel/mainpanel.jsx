44b11333930d6dcb39b81a650708bf8934742919
Dan Christopher <danchristopher12751@gmail.com>
2026-02-02 23:47:39 +0530

    created a input box

--- src/Components/MainPanel/mainpanel.jsx
+++ src/Components/MainPanel/mainpanel.jsx
@@ -1,10 +1,12 @@
 import Header from "./header.jsx";
 import "./mainpanel.css";
+import Taskinput from "./taskinput.jsx";
 
 const MainPanel = () => {
   return (
     <div className="main-panel">
       <Header />
+      <Taskinput />
     </div>
   );
 };
---
+++ src/Components/MainPanel/taskinput.jsx
@@ -0,0 +1,8 @@
+const Taskinput = () => {
+  return (
+    <div className="taskinputbox">
+      <input type="text" placeholder="Enter your task here..." />
+    </div>
+  );
+};
+export default Taskinput;
